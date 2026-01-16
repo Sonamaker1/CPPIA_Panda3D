24fa8e86bda6a6826f7e43bf819435107dd6b463
Winn W. <87080176+Sonamaker1@users.noreply.github.com>
2026-01-16 15:25:25 -0600

    whoops I'm tired

--- haxe/scripts/src/OpenPandaWindow.hx
+++ haxe/scripts/src/OpenPandaWindow.hx
@@ -6,10 +6,10 @@ class OpenPandaWindow {
 	public static function main() {
 		// Call convenience method on host-side Bridge:
 		Bridge.setBackground(0.1, 0.1, 0.2, 1.0);
-		Bridge.log("Opened Panda Window muthafucka!");
+		Bridge.log("Opened Panda Window!");
 		// Or call the extern shim directly (if you expose api pointer to script types)
 		// (Keeping it simple here.)
-		Bridge.log("SUPRISE");
+		Bridge.log("SURPRISE");
 
 	}
 }
